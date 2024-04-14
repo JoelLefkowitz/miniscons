55038648b32c4fc5f11d1ad9f90b80b3e25ba4d2
Joel Lefkowitz <joel.lefkowitz@teya.co>
2024-04-14 05:49:51 +0100

    Add thx scripts

--- test/test_tree.py
+++ test/test_tree.py
@@ -2,11 +2,7 @@ from src.services.compiler import compiler
 
 
 def test_compiler():
-    assert compiler(
-        "c++11",
-        ["all"],
-        ["deprecated-declarations"],
-    ) == [
+    assert compiler("c++11", ["all"], ["deprecated-declarations"],) == [
         "-std=c++11",
         "-Wall",
         "-Wno-deprecated-declarations",
