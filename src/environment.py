760f7e0bd762ac81f576dd577b4355bbc329b7b0
Joel Lefkowitz <joel.lefkowitz@teya.com>
2025-02-21 09:36:47 +0000

    Find SConscript_conandeps with a glob pattern

--- src/environment.py
+++ src/environment.py
@@ -2,16 +2,30 @@ import os
 import psutil
 from .constants import SCONS_FLAGS
 from emoji import emojize
+from glob import glob
 from SCons.Environment import Environment
 from SCons.Script import SConscript
 
 
+def environment(source: str | None = None) -> Environment:
+    if source is None:
+        matches = glob("**/SConscript_conandeps", recursive=True)
+
+        if matches:
+            source = matches[0]
+
+    return SConscript(source, must_exist=True)
+
+
 def conan(
     defines: list[str] | None = None,
-    source: str = "SConscript_conandeps",
+    source: str | None = None,
 ) -> Environment:
     if defines is None:
         defines = []
+
+    conandeps = environment(source)["conandeps"]
+    conandeps["CPPDEFINES"] += defines
 
     env = Environment(
         num_jobs=psutil.cpu_count(),
@@ -19,9 +33,6 @@ def conan(
         CXXCOMSTR=emojize(":wrench: Compiling $TARGET"),
         LINKCOMSTR=emojize(":link: Linking $TARGET"),
     )
-
-    conandeps = SConscript(source)["conandeps"]
-    conandeps["CPPDEFINES"] += defines
 
     env.MergeFlags(conandeps)
     return env
@@ -31,7 +42,7 @@ def packages(
     names: list[str],
     libs: list[str] | None = None,
     explicit: bool = False,
-    source: str = "SConscript_conandeps",
+    source: str | None = None,
 ) -> dict[str, list[str]]:
     if libs is None:
         libs = names.copy()
@@ -41,7 +52,7 @@ def packages(
 
     reduced = {"LIBS": libs}
 
-    for name, package in SConscript(source).items():
+    for name, package in environment(source).items():
         if name in names:
             for flag in SCONS_FLAGS:
                 reduced[flag] = reduced.get(flag, []) + package.get(flag, [])
